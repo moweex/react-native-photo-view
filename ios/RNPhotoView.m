57e8a3a5db9098a525b8363a81f59a9780edc78e
ereenyessamhanna <ereenyessam@hotmail.com>
2018-12-09 16:27:21 +0200

    Update RNPhotoView.m

--- ios/RNPhotoView.m
+++ ios/RNPhotoView.m
@@ -413,7 +413,7 @@
     _maxZoomScale = 5.0;
 
     // Setup
-    self.backgroundColor = [UIColor clearColor];
+    self.backgroundColor = [UIColor = whiteColor];
     self.delegate = self;
     self.decelerationRate = UIScrollViewDecelerationRateFast;
     self.showsVerticalScrollIndicator = YES;
@@ -423,12 +423,12 @@
     _tapView = [[MWTapDetectingView alloc] initWithFrame:self.bounds];
     _tapView.tapDelegate = self;
     _tapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    _tapView.backgroundColor = [UIColor clearColor];
+    _tapView.backgroundColor = [UIColor whiteColor];
     [self addSubview:_tapView];
 
     // Image view
     _photoImageView = [[MWTapDetectingImageView alloc] initWithFrame:self.bounds];
-    _photoImageView.backgroundColor = [UIColor clearColor];
+    _photoImageView.backgroundColor = [UIColor whiteColor];
     _photoImageView.contentMode = UIViewContentModeCenter;
     _photoImageView.tapDelegate = self;
     [self addSubview:_photoImageView];
